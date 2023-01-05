dbdc107149bab3e47c16267e9f1f7c95d0d5ad3f
SANGANA SWARNA <ucs20339@rmd.ac.in>
2023-01-05 19:36:16 +0530

    Add files via upload

--- Questions.js
+++ Questions.js
@@ -1,21 +1,13 @@
-import mongoose from 'mongoose'
+import express from 'express'
 
-const QuestionSchema = mongoose.Schema({
-    questionTitle: { type: String, required: "Question must have a title"},
-    questionBody: { type: String, required: "Question must have a body"},
-    questionTags: { type: [String], required: "Question must have a tags"},
-    noOfAnswers: { type: Number, default: 0},
-    upVote: { type: [String], default: []},
-    downVote: { type: [String], default: []},
-    userPosted: { type: String, required: "Question must have an author"},
-    userId: { type: String},
-    askedOn: { type: Date, default: Date.now},
-    answer: [{
-        answerBody: String,
-        userAnswered: String,
-        userId: String,
-        answeredOn: { type: Date, default: Date.now},
-    }]
-})
+import { AskQuestion, getAllQuestions, deleteQuestion, voteQuestion } from '../controllers/Questions.js' 
+import auth from '../middleware/auth.js'
 
-export default mongoose.model("Question", QuestionSchema)
+const router = express.Router()
+
+router.post('/Ask', auth, AskQuestion)
+router.get('/get', getAllQuestions)
+router.delete('/delete/:id', auth, deleteQuestion );
+router.patch('/vote/:id',  auth, voteQuestion);
+
+export default router
